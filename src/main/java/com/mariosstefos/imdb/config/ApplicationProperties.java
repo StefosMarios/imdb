0fa3d5f07d991a10e4cd37d24a0506dc282acaa2
Marios Stefos <marios.stefos@incelligent.net>
2022-02-03 21:42:20 +0200

    Initial version of imdb

---
+++ src/main/java/com/mariosstefos/imdb/config/ApplicationProperties.java
@@ -0,0 +1,12 @@
+package com.mariosstefos.imdb.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Properties specific to Imdb.
+ * <p>
+ * Properties are configured in the {@code application.yml} file.
+ * See {@link tech.jhipster.config.JHipsterProperties} for a good example.
+ */
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+public class ApplicationProperties {}
---
+++ src/main/java/com/mariosstefos/imdb/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.mariosstefos.imdb.service.dto;
